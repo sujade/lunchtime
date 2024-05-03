62338335f9e4ad1177d2a05eaf95f729b9aec2ad
sujade <dnmzsu@gmail.com>
2024-05-03 14:22:54 +0200

    rebranding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './globals.css'
-import Hero from "@/app/components/landing/Hero";
+import Hero from "@/app/components/Landing/Hero";
 
 export default function Home() {
     return (
